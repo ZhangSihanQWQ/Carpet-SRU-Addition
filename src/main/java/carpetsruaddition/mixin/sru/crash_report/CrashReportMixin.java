e35da0d50109693b8c0d3ac1d666224d9c2c6900
ZhangSihanQWQ <michaelaugenstern0@gmail.com>
2026-07-01 22:54:58 +0800

    crashReportCarpetRules

--- src/main/java/carpetsruaddition/mixin/sru/crash_report/CrashReportMixin.java
+++ src/main/java/carpetsruaddition/mixin/sru/crash_report/CrashReportMixin.java
@@ -2,28 +2,19 @@ package carpetsruaddition.mixin.sru.crash_report;
 
 import carpetsruaddition.CarpetSettings;
 import carpetsruaddition.crashreport.CrashReportCarpetRules;
-import net.minecraft.util.crash.CrashReportSection;
 import net.minecraft.util.crash.CrashReport;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
 @Mixin(CrashReport.class)
 public abstract class CrashReportMixin {
-    @Shadow
-    @Final
-    private List<CrashReportSection> otherSections;
-
     @Unique
     private boolean carpetSruAddition$addedCarpetRules;
 
-    @Inject(method = "addDetails", at = @At("HEAD"))
+    @Inject(method = "addDetails", at = @At("TAIL"))
     private void carpetSruAddition$addCarpetRules(StringBuilder crashReportBuilder, CallbackInfo ci) {
         if (this.carpetSruAddition$addedCarpetRules || !CarpetSettings.crashReportCarpetRules) {
             return;
@@ -31,9 +22,9 @@ public abstract class CrashReportMixin {
 
         this.carpetSruAddition$addedCarpetRules = true;
         try {
-            CrashReportSection section = new CrashReportSection("Carpet Rules");
-            section.add("Modified rules", CrashReportCarpetRules.createReport());
-            this.otherSections.add(section);
+            crashReportBuilder.append("\n\n-- Carpet Rules --\n");
+            crashReportBuilder.append("Details:");
+            crashReportBuilder.append(CrashReportCarpetRules.createReport());
         } catch (Throwable ignored) {
         }
     }
