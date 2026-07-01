944dc7878799b308de6213757ac4d18d3e722310
ZhangSihanQWQ <michaelaugenstern0@gmail.com>
2026-07-01 23:43:56 +0800

    crashReportCarpetRules

--- src/main/java/carpetsruaddition/mixin/sru/crash_report/SystemDetailsMixin.java
+++ src/main/java/carpetsruaddition/mixin/sru/crash_report/SystemDetailsMixin.java
@@ -1,31 +1,22 @@
 package carpetsruaddition.mixin.sru.crash_report;
 
 import carpetsruaddition.crashreport.CrashReportCarpetRules;
-import net.minecraft.util.SystemDetails;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.util.SystemDetails;
+
 @Mixin(SystemDetails.class)
 public abstract class SystemDetailsMixin {
-    private static final String CARPET_RULES_MARKER = "\n\tCarpet Rules:";
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void carpetSruAddition$addCarpetRules(CallbackInfo ci) {
-        try {
-            if (CrashReportCarpetRules.isRuleEnabled()) {
-                ((SystemDetails) (Object) this).addSection("Carpet Rules", CrashReportCarpetRules.reportForCrash());
-            }
-        } catch (Throwable ignored) {
-        }
-    }
+    private static final String CARPET_RULES_HEADER = "\n\n-- Carpet Rules --\nDetails:";
 
     @Inject(method = "writeTo", at = @At("TAIL"))
     private void carpetSruAddition$writeCarpetRules(StringBuilder stringBuilder, CallbackInfo ci) {
         try {
-            if (CrashReportCarpetRules.isRuleEnabled() && stringBuilder.indexOf(CARPET_RULES_MARKER) < 0) {
-                stringBuilder.append(CARPET_RULES_MARKER).append(' ').append(CrashReportCarpetRules.reportForCrash());
+            if (CrashReportCarpetRules.isRuleEnabled() && stringBuilder.indexOf(CARPET_RULES_HEADER) < 0) {
+                stringBuilder.append(CARPET_RULES_HEADER).append(CrashReportCarpetRules.reportForCrash());
             }
         } catch (Throwable ignored) {
         }
