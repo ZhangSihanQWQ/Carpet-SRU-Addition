02b7278e9356da4e954117b6bf1af9293da37b19
ZhangSihanQWQ <michaelaugenstern0@gmail.com>
2026-07-02 00:01:49 +0800

    crashReportCarpetRules

--- src/main/java/carpetsruaddition/mixin/sru/crash_report/SystemDetailsMixin.java
+++ src/main/java/carpetsruaddition/mixin/sru/crash_report/SystemDetailsMixin.java
@@ -2,6 +2,7 @@ package carpetsruaddition.mixin.sru.crash_report;
 
 import carpetsruaddition.crashreport.CrashReportCarpetRules;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,7 +11,20 @@ import net.minecraft.util.SystemDetails;
 
 @Mixin(SystemDetails.class)
 public abstract class SystemDetailsMixin {
+    @Unique
     private static final String CARPET_RULES_HEADER = "\n\n-- Carpet Rules --\nDetails:";
+    @Unique
+    private static final String CARPET_RULES_DETAIL = "Carpet Rules";
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void carpetSruAddition$addCarpetRulesFallback(CallbackInfo ci) {
+        try {
+            if (CrashReportCarpetRules.isRuleEnabled()) {
+                ((SystemDetails) (Object) this).addSection(CARPET_RULES_DETAIL, CrashReportCarpetRules.reportForCrash());
+            }
+        } catch (Throwable ignored) {
+        }
+    }
 
     @Inject(method = "writeTo", at = @At("TAIL"))
     private void carpetSruAddition$writeCarpetRules(StringBuilder stringBuilder, CallbackInfo ci) {
