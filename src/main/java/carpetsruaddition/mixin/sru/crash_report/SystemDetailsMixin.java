40551b952912d81f47796745a85973281a0bdf33
ZhangSihanQWQ <michaelaugenstern0@gmail.com>
2026-07-01 23:36:10 +0800

    crashReportCarpetRules

--- src/main/java/carpetsruaddition/mixin/sru/crash_report/SystemDetailsMixin.java
+++ src/main/java/carpetsruaddition/mixin/sru/crash_report/SystemDetailsMixin.java
@@ -9,11 +9,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SystemDetails.class)
 public abstract class SystemDetailsMixin {
+    private static final String CARPET_RULES_MARKER = "\n\tCarpet Rules:";
+
     @Inject(method = "<init>", at = @At("TAIL"))
     private void carpetSruAddition$addCarpetRules(CallbackInfo ci) {
         try {
             if (CrashReportCarpetRules.isRuleEnabled()) {
                 ((SystemDetails) (Object) this).addSection("Carpet Rules", CrashReportCarpetRules.reportForCrash());
+            }
+        } catch (Throwable ignored) {
+        }
+    }
+
+    @Inject(method = "writeTo", at = @At("TAIL"))
+    private void carpetSruAddition$writeCarpetRules(StringBuilder stringBuilder, CallbackInfo ci) {
+        try {
+            if (CrashReportCarpetRules.isRuleEnabled() && stringBuilder.indexOf(CARPET_RULES_MARKER) < 0) {
+                stringBuilder.append(CARPET_RULES_MARKER).append(' ').append(CrashReportCarpetRules.reportForCrash());
             }
         } catch (Throwable ignored) {
         }
